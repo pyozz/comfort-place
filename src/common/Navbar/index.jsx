e83f658f6934eb97dc43a495b78c9262a8c8e35a
PYO SEUNGYEON <pyo.seungyeonn@gmail.com>
2024-02-09 21:39:54 +0900

    🚧 모바일 Navbar 컴포넌트 레이아웃 구현

---
+++ src/common/Navbar/index.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import logo from "@/assets/images/logo.svg";
+
+import * as S from "./style";
+
+function Navbar() {
+  return (
+    <S.NavContainer>
+      <div className="nav-center">
+        <div className="nav-header">
+          <Link to="/">
+            <img src={logo} alt="편안플레이스" />
+          </Link>
+
+          {/* TODO: 비로그인 -> 로그인 버튼, 로그인 -> 위시리스트, 장바구니 버튼 */}
+          <button type="button">로그인</button>
+        </div>
+      </div>
+    </S.NavContainer>
+  );
+}
+
+export default Navbar;
---
+++ src/common/Navbar/style.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const NavContainer = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 40px;
+  border-bottom: 1px solid #dedede;
+
+  .nav-center {
+    width: 90vw;
+    margin: 0 auto;
+  }
+
+  .nav-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    a {
+      display: block;
+      width: 90px;
+
+      img {
+        display: block;
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+`;
