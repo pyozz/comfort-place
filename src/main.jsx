e16ed5affd54e7deb8cab61d6bfcb799b57a52b2
PYO SEUNGYEON <pyo.seungyeonn@gmail.com>
2024-02-09 19:55:11 +0900

    🚧 홈페이지 라우터 경로 설정

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-import GlobalStyle from "./styles/globalStyle.js";
 import { Reset } from "styled-reset";
+import { BrowserRouter } from "react-router-dom";
+
+import GlobalStyle from "./styles/globalStyle.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Reset />
-    <GlobalStyle />
+    <BrowserRouter>
+      <Reset />
+      <GlobalStyle />
 
-    <App />
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "@/common/Navbar";
+
+function HomePage() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default HomePage;
